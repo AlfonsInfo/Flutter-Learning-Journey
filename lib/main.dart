a31a53612792550d225e7d68aca6bbdf2b6e5857
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-01-29 23:46:32 +0700

    stateful widget on progress

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,26 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-          title: Center(
-        child: Text("Latihan Container"),
-      )),
-      body: Container(
-        color: Colors.redAccent,
-        margin: EdgeInsets.only(bottom: 50),
-        padding: EdgeInsets.fromLTRB(10,20,30,40),
-        child: Container(
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),gradient: LinearGradient(colors: <Color>[
-          Colors.amber,
-          Colors.blue
-        ],begin: Alignment.topLeft, end: Alignment.bottomRight))),
-      ),
-    ));
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
+class _MyAppState extends State<MyApp> {
+  void tekanTombol(){}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: AppBar(title:Text("Stateful Widget"),),
+      body: Center(child: Column(
+        children: <Widget>[
+          Text("12"),
+          ElevatedButton(onPressed: tekanTombol, child: Text("Tambah Bilangan"))
+        ],
+      ),)),
+    );
+  }
+}
 //STL -> Shortcut stateless widget.
