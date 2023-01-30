bedad91b52fbd78bc86adbc0313102d4174d513b
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-01-30 11:08:59 +0700

    font

--- lib/main.dart
+++ lib/main.dart
@@ -20,20 +20,8 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Center(child: Text("Anonymous Method"))),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(message),
-              // Method hanya dipanggil 1x saja
-              ElevatedButton(onPressed: ()=>{
-                //setState merupakan anonymous method, sebelum dia dijalankan dia membutuhkan method lain terlebih dahulu untuk dijalankan ( sebelum dia updatae tampilan)
-                setState(() {
-                message = "Tombol sudah ditekan";
-                })
-              },child: Text("Tekan Saya"))
-            ],) ),
+        appBar: AppBar(title: Center(child: Text("TextStyle"))),
+        body: Center(child: Text("ini adalah Text", style: TextStyle(fontFamily: "Good", fontSize: 50 ,fontWeight:FontWeight.bold )),),
       ),
     );
   }
