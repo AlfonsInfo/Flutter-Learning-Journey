1a43d0b22590ff6391e46bce4a9c29a45165bc56
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-01-31 20:44:39 +0700

    card Widget + Extract Method

--- lib/main.dart
+++ lib/main.dart
@@ -20,33 +20,44 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.green,
       appBar: AppBar(
-          leading: Icon(Icons.adb, color: Colors.white),
-          title: Text("AppBar Example"),
-          actions: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.settings)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.exit_to_app)),
+        leading: Icon(Icons.adb, color: Colors.white),
+        title: Text("AppBar Example"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app)),
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight),
+            // image: DecorationImage(image: AssetImage("assets/pattern.jpg"), fit: BoxFit.none, repeat: ImageRepeat.repeat) //gunakan pattern semi transparan
+          ),
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            buildCard(Icons.account_box, "Account Box"),
+            buildCard(Icons.adb, "Android"),
+            buildCard(Icons.access_alarm, "Arm"),
           ],
-          flexibleSpace: Container(decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [Color(0xff0096ff),Color(0xff6610f2)],begin: FractionalOffset.topLeft, end: FractionalOffset.bottomRight),
-          // image: DecorationImage(image: AssetImage("assets/pattern.jpg"), fit: BoxFit.none, repeat: ImageRepeat.repeat) //gunakan pattern semi transparan
-          ),
-          ),          
-          ),
-      body: Center(
-        child: Row(children: [
-          Spacer(
-            flex: 2,
-          ),
-          Container(width: 50, height: 50, color: Colors.blueAccent),
-          Spacer(),
-          Container(width: 50, height: 50, color: Colors.blueAccent),
-          Spacer(),
-          Container(width: 50, height: 50, color: Colors.blueAccent),
-          Spacer(),
-        ]),
+        ),
       ),
     ));
+  }
+
+  Card buildCard(IconData iconData, String text) {
+    return Card(
+            elevation: 5,
+            child: Row(
+              children: [Container(margin:EdgeInsets.all(5) ,child: Icon(iconData)), Text(text)],
+            ),
+          );
   }
 }
 //STL -> Shortcut stateless widget.
