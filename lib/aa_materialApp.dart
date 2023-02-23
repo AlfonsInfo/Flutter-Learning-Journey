6779a4a4f60a9bea2004f5a1637b51d526a7a4cf
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-02-23 13:55:12 +0700

    Scaffold

---
+++ lib/aa_materialApp.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Material App is a Grand daddy',textAlign: TextAlign.center,),/*style: TextStyle(fontSize: 30) */),
+    ),
+  );
+}
+  
