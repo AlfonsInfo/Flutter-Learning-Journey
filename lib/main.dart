1e09b958f9eb452be3777c40328539a3a0428f71
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-02-23 12:43:04 +0700

    Create Apps from Scratch : Material App

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Material App is a Grand daddy',textAlign: TextAlign.center,),/*style: TextStyle(fontSize: 30) */),
+    ),
+  );
+}
