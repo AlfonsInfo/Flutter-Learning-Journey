463e9514051d76dbd601d9e724d3a99c40deac24
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-02-25 18:30:24 +0700

    mend

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,78 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          //* Text Berada ditengah layar
-          child: Text('Hello World!'),
+    return MaterialApp(
+        home: Scaffold(
+            body: ListView(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            createBlock(Colors.blueAccent),
+            createBlock(Colors.blueAccent),
+          ],
         ),
-      ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            createBlock(Colors.blueAccent),
+            createBlock(Colors.blueAccent),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+          children: [
+            createBlock(Colors.blueAccent),
+            createBlock(Colors.blueAccent),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+          children: [
+            createBlock(Colors.blueAccent),
+            createBlock(Colors.blueAccent),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+          children: [
+            createBlock(Colors.blueAccent),
+            createBlock(Colors.blueAccent),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+          children: [
+            createBlock(Colors.blueAccent),
+            createBlock(Colors.blueAccent),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+          children: [
+            createBlock(Colors.blueAccent),
+            createBlock(Colors.blueAccent),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('1'),
+            Text('1'),
+            Text('1'),
+            Text('1'),
+          ],
+        )
+      ],
+    )));
+  }
+
+  Container createBlock(Color colorBlock) {
+    return Container(
+      decoration: BoxDecoration(color: colorBlock),
+      width: 100,
+      height: 100,
+      child: const Center(child: Text('Text Center')),
     );
   }
 }
